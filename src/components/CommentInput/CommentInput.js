5aa8efaf17f7c2b5dc2cee2ad516ce6580fad119
Kirill Tchentsov <kirill.develops@gmail.com>
2022-02-24 21:25:48 -0500

    styled CommentInput component @mobile

--- src/components/CommentInput/CommentInput.js
+++ src/components/CommentInput/CommentInput.js
@@ -1,14 +1,17 @@
 import avatar from "../../assets/Images/Mohan-muruge.jpg"
+import "./CommentInput.scss"
 
 const CommentInput = (props) => {
   return (
     <section className="comment-input">
-      <h3 className="comment-input__title">{props.data.comments.length} Comments</h3>
+      <h3 className="comment-input__header">{props.data.comments.length} Comments</h3>
       <form className="comment-input__form" id="newComment">
         <img className="comment-input__avatar" htmlFor="userComment" src={avatar} alt=""></img>
-        <label className="comment-input__label" htmlFor="userComment">Join the Conversation</label>
-        <textarea className="comment-input__field" id="userComment" name="userComment" placeholder="Add a new comment"></textarea>
-        <button className="comment-input__button" type="submit">COMMENT</button>
+        <div className="comment-input__right">
+          <label className="comment-input__label" htmlFor="userComment">JOIN THE CONVERSATION</label>
+          <textarea className="comment-input__field" id="userComment" name="userComment" placeholder="Add a new comment" rows="5"></textarea>
+          <button className="comment-input__button" type="submit">COMMENT</button>
+        </div>
       </form>
     </section>
   );
