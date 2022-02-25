7c52a8925009c8c7e83a29641af5986cd9488236
Kirill Tchentsov <kirill.develops@gmail.com>
2022-02-24 20:43:42 -0500

    styled MediaDetails component @mobile

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import videos from './data/videos.json'
-import videoDetails from './data/video-details.json'
+import videos from './data/videos.json';
+import videoDetails from './data/video-details.json';
 import './styles/App.scss';
 
 import Nav from './components/Nav/Nav';
 import Media from './components/Media/Media';
-import MediaDetails from './components/MediaDetails/MediaDetails'
+import MediaDetails from './components/MediaDetails/MediaDetails';
 import CommentInput from './components/CommentInput/CommentInput';
 import CommentList from './components/CommentList/CommentList';
 import NextVideo from './components/NextVideo/NextVideo';
@@ -16,7 +16,7 @@ class App extends React.Component {
     videos: videos,
     videoDetails: videoDetails
   }
-
+  
   render() {
     console.log(videoDetails);
     return (
@@ -27,7 +27,6 @@ class App extends React.Component {
         <CommentInput data={this.state.videoDetails[0]}></CommentInput>
         <CommentList data={this.state.videoDetails[0]}></CommentList>
         <NextVideo vidArr={this.state.videoDetails}></NextVideo>
-
       </div>
     )
   };
--- src/components/MediaDetails/MediaDetails.js
+++ src/components/MediaDetails/MediaDetails.js
@@ -6,10 +6,10 @@ const MediaDetails = (props) => {
     <div className="media-highlights">
       <h1 className="media-highlights__title">{props.data.title}</h1>
       <ul className="media-detail">
-        <li className="media-detail__author">{props.data.channel}</li>
-        <li className="media-detail__date">{props.data.timestamp}</li>
-        <li className="media-detail__view">{props.data.views}</li>
-        <li className="media-detail__likes">{props.data.likes}</li>
+        <li className="media-detail__label--special">{props.data.channel}</li>
+        <li className="media-detail__label">{props.data.views}</li>
+        <li className="media-detail__label">{props.data.timestamp}</li>
+        <li className="media-detail__label">{props.data.likes}</li>
       </ul>
       <p className="media-highlights__content">{props.data.description}</p>
     </div>
