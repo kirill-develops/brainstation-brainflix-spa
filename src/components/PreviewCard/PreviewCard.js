400807dc192807673d965369bec826836b9346bd
Kirill Tchentsov <kirill.develops@gmail.com>
2022-02-24 22:39:22 -0500

    styled NextVideo and PreviewCard Components @media

--- src/components/PreviewCard/PreviewCard.js
+++ src/components/PreviewCard/PreviewCard.js
@@ -1,12 +1,14 @@
 import './PreviewCard.scss'
 
-const PreviewCard = ({title, image, author}) => {
+const PreviewCard = ({ title, image, author }) => {
 
   return (
     <div className='preview-card'>
       <img className='preview-card__media' alt={title} src={image}></img>
-      <h3 className='preview-card__title'>{title}</h3>
-      <h4 className='preview-card__author'>{author}</h4>
+      <div className='preview-card__right'>
+        <h3 className='preview-card__title'>{title}</h3>
+        <h4 className='preview-card__author'>{author}</h4>
+      </div>
     </div>
   )
 }
