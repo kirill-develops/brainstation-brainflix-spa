7f27b56a1c0fbe5935bb9731421bb163d4a394c3
Kirill Tchentsov <kirill.develops@gmail.com>
2022-02-26 10:52:58 -0500

    added icon to search field in nav

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -9,7 +9,7 @@ const Nav = () => {
       <img src={logo} className="nav__logo" alt="site logo"></img>
       <form className="nav__form">
         <div className="nav__mobile-form">
-          <input type="text" name="search" className="nav__search" placeholder="Search"></input>
+          <input type="search" name="search" className="nav__search" placeholder="Search"></input>
           <button className="nav__button--tablet">UPLOAD</button>
           <img src={avatar} className="nav__avatar" alt="user avatar" htmlFor="search"></img>
         </div>
