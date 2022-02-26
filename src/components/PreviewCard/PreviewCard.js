52615e623aff1ff528965200d69b6fbe909fa779
Kirill Tchentsov <kirill.develops@gmail.com>
2022-02-26 16:16:18 -0500

    styled sit @desktop & added hover on next-vid cards

--- src/components/PreviewCard/PreviewCard.js
+++ src/components/PreviewCard/PreviewCard.js
@@ -8,7 +8,7 @@ const PreviewCard = ({ title, image, author, handleVideoChange, id }) => {
   }
 
   return (
-    <a className='preview-card' onClick={(event) => { clickHandler(event, id) }}>
+    <a className='preview-card' href={title} onClick={(event) => { clickHandler(event, id) }}>
       <img className='preview-card__media' alt={title} src={image}></img>
       <div className='preview-card__right'>
         <h3 className='preview-card__title'>{title}</h3>
