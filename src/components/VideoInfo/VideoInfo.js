b54a79669bff17f34781f3de5233a2bcf8e75d35
Kirill Tchentsov <kirill.develops@gmail.com>
2022-03-05 18:40:28 -0500

    renamed mediaHighlights to videoInfo

---
+++ src/components/VideoInfo/VideoInfo.js
@@ -0,0 +1,24 @@
+import VideoDetail from '../VideoDetail/VideoDetail'
+import './VideoInfo.scss';
+
+const VideoInfo = ({ videoObj }) => {
+  
+  const { title, timestamp, channel, views, likes, description } = videoObj
+
+  return (
+    <div className="video-info">
+      <h1 className="video-info__title">{title}</h1>
+
+      <VideoDetail
+        timestamp={timestamp}
+        channel={channel}
+        views={views}
+        likes={likes}
+      />
+
+      <p className="video-info__content">{description}</p>
+    </div>
+  )
+}
+
+export default VideoInfo;
