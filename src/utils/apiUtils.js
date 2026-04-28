c3907e690ad749f50c61163382b04ca8b5186450
Kirill Tchentsov <kirill.develops@gmail.com>
2026-04-28 18:31:28 -0400

    implemented env check and udpated video URL

--- src/utils/apiUtils.js
+++ src/utils/apiUtils.js
@@ -1,35 +1,39 @@
 import axios from "axios";
 
-const BASE_URL = process.env.REACT_APP_BASE_URL;
-console.log(BASE_URL);
-
+const BASE_URL =
+   process.env.NODE_ENV === "development"
+      ? process.env.REACT_APP_BASE_URL_DEV
+      : process.env.REACT_APP_BASE_URL_PROD;
 
 const getAllReq = {
-  baseURL: BASE_URL,
-  method: 'get',
-  url: '/videos',
-}
+   baseURL: BASE_URL,
+   method: "get",
+   url: "/videos",
+};
 
 const apiUtils = {
-  getAll: () => axios(getAllReq),
-  getVideoById: (id) => axios.get(`${BASE_URL}/videos/${id}`),
-  postVideoComment: (id, name, comment) => axios.post(`${BASE_URL}/videos/${id}/comments`, {
-    'name': name,
-    'comment': comment,
-    'id': id
-  }),
-  postVideo: (title, description, video) => axios.post(`${BASE_URL}/videos/`, {
-    'title': title,
-    'channel': 'Mohan Muruge',
-    'image': video,
-    'description': description,
-    'video': "https://project-2-api.herokuapp.com/stream"
-  }),
-  likeVideo: (videoId, liked) => axios.put(`${BASE_URL}/videos/${videoId}/likes/`, {
-    'liked': liked
-  }),
-  deleteVideoComment: (videoId, commentId) => axios.delete(`${BASE_URL}/videos/${videoId}/comments/${commentId}`)
-}
-
+   getAll: () => axios(getAllReq),
+   getVideoById: (id) => axios.get(`${BASE_URL}/videos/${id}`),
+   postVideoComment: (id, name, comment) =>
+      axios.post(`${BASE_URL}/videos/${id}/comments`, {
+         name: name,
+         comment: comment,
+         id: id,
+      }),
+   postVideo: (title, description, video) =>
+      axios.post(`${BASE_URL}/videos/`, {
+         title: title,
+         channel: "Mohan Muruge",
+         image: video,
+         description: description,
+         video: "https://archive.org/download/kikTXNL6MvX6ZpRXM/kikTXNL6MvX6ZpRXM.mp4",
+      }),
+   likeVideo: (videoId, liked) =>
+      axios.put(`${BASE_URL}/videos/${videoId}/likes/`, {
+         liked: liked,
+      }),
+   deleteVideoComment: (videoId, commentId) =>
+      axios.delete(`${BASE_URL}/videos/${videoId}/comments/${commentId}`),
+};
 
 export default apiUtils;
